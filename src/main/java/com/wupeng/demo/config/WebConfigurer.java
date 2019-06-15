4844872caf3d7d0c954ad878a950b81c5e655f51
kingpeng <2724418180@qq.com>
2019-06-15 09:43:15 +0800

    commit  test interceptor config

---
+++ src/main/java/com/wupeng/demo/config/WebConfigurer.java
@@ -0,0 +1,21 @@
+package com.wupeng.demo.config;
+
+import com.wupeng.demo.interceptor.PassInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        /**
+         * addPathPatterns：需要拦截的访问路径
+         * excludePathPatterns：不需要拦截的路径，String数组类型可以写多个用","分割
+         * */
+        registry.addInterceptor(new PassInterceptor()).excludePathPatterns("/static/**");
+    }
+
+
+}
--- src/main/java/com/wupeng/demo/interceptor/PassInterceptor.java
+++ src/main/java/com/wupeng/demo/interceptor/PassInterceptor.java
@@ -1,6 +1,7 @@
 package com.wupeng.demo.interceptor;
 
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +17,23 @@ public class PassInterceptor implements HandlerInterceptor {
             throws Exception {
         String url = request.getRequestURI();
         System.out.println(url);
+        if(url.indexOf("getIndex")==-1){
+            response.sendRedirect("orderInfo.html");
+            return false;
+        }
         return true;
     }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle...");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("afterCompletion...");
+    }
+
+
 
 }
